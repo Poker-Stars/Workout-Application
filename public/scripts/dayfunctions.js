eb3131e448ec0f5996dd651beddfacacafe059e7
Alexander Gonzales <acgonzales42@gmail.com>
2020-11-29 16:35:54 -0700

    Small tweaks

--- public/scripts/dayfunctions.js
+++ public/scripts/dayfunctions.js
@@ -1,3 +1,21 @@
-function days_one(_workoutDays, goal, shoulders, legs, forearms, body_fat, ) {
+function days1(goal, workoutDays, dayCount, exerciseTime, bodyFat, shoulders, legs, arms, chest) {
+    
+}
+function days2(goal, workoutDays, dayCount, exerciseTime, bodyFat, shoulders, legs, arms, chest) {
+
+}
+function days3(goal, workoutDays, dayCount, exerciseTime, bodyFat, shoulders, legs, arms, chest) {
+
+}
+function days4(goal, workoutDays, dayCount, exerciseTime, bodyFat, shoulders, legs, arms, chest) {
+    
+}
+function days5(goal, workoutDays, dayCount, exerciseTime, bodyFat, shoulders, legs, arms, chest) {
+
+}
+function days6(goal, workoutDays, dayCount, exerciseTime, bodyFat, shoulders, legs, arms, chest) {
+    
+}
+function calcPrio(shoulders, legs, arms, chest) {
 
 }
--- public/scripts/workout.js
+++ public/scripts/workout.js
@@ -3,23 +3,18 @@ function calculateWorkout() {
     const ref = database.ref('users');
     const auth = firebase.auth();
     let _workoutDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    var bodytype;
     var dayCount = 0;
-    var days;
     var goal;
     var shoulders;
     var legs;
-    var forearms;
-    var body_fat;
-    var body_shape;
-    var wrist;
-    var weight_gain;
+    var arms;
+    var bodyFat;
     var chest;
-    var user_exercises = new Array();
-    var exercise_specifics = new Array();
+    var userExercises = new Array();
+    var exerciseSpecifics = new Array();
+    var exerciseTime;
 
     firebase.auth().onAuthStateChanged((user) => {
-        window.alert("Hiya1");
         if (user) {
             var useruid = user.uid;
             console.log(useruid);
@@ -28,7 +23,6 @@ function calculateWorkout() {
             window.alert("No user is signed in");
         }
         firebase.database().ref('users/'+useruid).once('value').then(function (snapshot){
-            window.alert("Hiya2");
 
             goal = snapshot.val().goal;
 
@@ -49,13 +43,17 @@ function calculateWorkout() {
                 i++;
             });
             var workoutDays = _workoutDays.slice(0, dayCount);
-            body_fat = snapshot.val().bodyfat;
+            exerciseTime = snapshot.val().time;
+            bodyFat = snapshot.val().bodyfat;
             shoulders = snapshot.val().shoulders;
             legs = snapshot.val().legs;
             arms = snapshot.val().arms; 
             chest = snapshot.val().chest;
             window.alert("dayCount: " + dayCount);
             window.alert("days: " + workoutDays);
+
+            
+
         });
     });
 }
